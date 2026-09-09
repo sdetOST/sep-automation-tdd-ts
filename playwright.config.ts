992aa01bd22ac6a8c4716034aefd83052c039ef8
Goodfellas21 <akinhomer@gmail.com>
2026-09-09 14:54:58 -0400

    commmit config updates

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,24 +7,33 @@ dotenv.config();
 
 export default defineConfig({
   testDir: './tests',
+  /* Run tests in files in parallel */
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
+  forbidOnly: !!process.env.CI,   
+  /* Retry on CI only */
   retries: 1,
-  workers: 1,
-  reporter: 'html',
+  /* Opt out of parallel tests on CI. */
+  workers: 1,  
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',  
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    baseURL: "",
-    trace: 'on-first-retry',
     headless: process.env.CI ? true : false,
+    baseURL: "",
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+    // screenshot: "on-first-failure",
+    // video: {mode: "on-first-retry", size: {width: 1900, height: 1080}},
   },
 
   projects: [
 
     {
-      name: 'Google Chrome',
+      name: 'chromium',
       use: {
-        channel: process.env.CI ? undefined : 'chrome',
+        // channel: process.env.CI ? undefined : 'chrome',
         // In headed mode, set viewport to null and launch maximized so it runs in full screen
+        browserName: 'chromium',
         viewport: process.env.CI ? { width: 1920, height: 1080 } : null,
         deviceScaleFactor: process.env.CI ? 1 : undefined,
         launchOptions: {
@@ -50,17 +59,27 @@ export default defineConfig({
         viewport: { width: 1800, height: 1000 },
       },
     },
-*/
+    {
+      name: 'Microsoft Edge',
+      use: { 
+        ...devices['Desktop Edge'],
+        channel: 'msedge',
+        viewport: {width: 1900, height: 1080} 
+      },
+    },
 
-/* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+//=====================================
+//----Test against mobile viewports----   
+//-------------------------------------   
+    {
+      name: 'Mobile Chrome',
+      use: { ...devices['Pixel 5'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 12'] },
+    }, 
+*/
 
   ],
 });
